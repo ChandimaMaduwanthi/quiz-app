1fde6bd67241482471fe8a2d3464c7bd186bf17f
ChandimaMaduwanthi <48376228+ChandimaMaduwanthi@users.noreply.github.com>
2020-10-10 22:15:52 +0530

    js file updated

--- script.js
+++ script.js
@@ -35,4 +35,43 @@ const quizData = [
         d: 'none of the above',
         correct: 'd',
     }
-]
+];
+
+const question = document.getElementById('question');
+const a_text = document.getElementById('a_text');
+const b_text = document.getElementById('b_text');
+const c_text = document.getElementById('c_text');
+const d_text = document.getElementById('d_text');
+const submitBtn = document.getElementById('submit');
+
+let currentQuiz = 0;
+
+loadQuiz();
+
+function loadQuiz(){
+    const currentQuizData = quizData[currentQuiz];
+    
+    question.innerHTML = currentQuizData.question;
+    a_text.innerHTML = currentQuizData.a;
+    b_text.innerHTML = currentQuizData.b;
+    c_text.innerHTML = currentQuizData.c;
+    d_text.innerHTML = currentQuizData.d;
+
+}
+
+submitBtn.addEventListener('click' , () =>{
+    currentQuiz++;
+    if(currentQuiz < quizData.length){
+        loadQuiz();
+    }else{
+        alert("You finished!");
+    }    
+}
+);
+
+function getSelected(){
+    const answers = document.getElementsByName('answer');
+    answers.forEach((answer) => {
+        console.log(answer);
+    })
+}
