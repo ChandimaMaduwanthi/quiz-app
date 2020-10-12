40401d8261ec09c4cc3a8334030417a2b8d695e2
ChandimaMaduwanthi <48376228+ChandimaMaduwanthi@users.noreply.github.com>
2020-10-12 20:49:31 +0530

    js file updated

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const quizData = [
     {
-        question: 'How old is Florin?',
-        a: '10',
-        b: '17',
-        c: '26',
-        d: '110',
+        question: 'Which year were you born?',
+        a: '1996',
+        b: '1997',
+        c: '1998',
+        d: '1999',
         correct: 'c',
     },{
         question: 'What is the most used programming language in 2019?',
@@ -28,12 +28,12 @@ const quizData = [
         d: 'Helicopters Terminals Motorboats Lamborginis',
         correct: 'a',
     },{
-        question: 'What year was JavaScript launched?',
+        question: 'Which year was JavaScript launched?',
         a: '1996',
         b: '1995',
         c: '1994',
         d: 'none of the above',
-        correct: 'd',
+        correct: 'b',
     }
 ];
 
